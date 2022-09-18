5d42bedcc5c235c77de658ec4167b502ec726390
nickbvr <kkonfedrat@gmail.com>
2022-09-18 13:07:38 +0300

    Initial commit

--- config/base.config.js
+++ config/base.config.js
@@ -1,2 +1,4 @@
 export const apiKey = '2ba025e8d1msh2da01cada98ffcbp1f07b7jsn9cd0bc0bf6ef';
 export const apiUrl = 'https://jspell-checker.p.rapidapi.com/check';
+
+// Additional api-key if the existing does not work: a14fe16aa9msh53bc90ed5212a0ep1aceeejsn127111a6309c
